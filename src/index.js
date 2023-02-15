58aac7a326c3a671b7428527f1cd871ec2afdddb
Noseble <manydreamcometure@gmail.com>
2023-02-15 15:32:20 +0800

    add header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.module.css';
-import Main from './components/Main';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.module.css";
+import Main from "./components/Main";
+import Header from "./components/Header/Header";
 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <Main />
   </React.StrictMode>
 );
-
