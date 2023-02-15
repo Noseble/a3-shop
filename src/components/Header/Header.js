775c15e7fc6ecd0c48ed6c173f95544f44d0b47b
Noseble <manydreamcometure@gmail.com>
2023-02-15 15:35:32 +0800

    add header footer

---
+++ src/components/Header/Header.js
@@ -0,0 +1,56 @@
+import style from "./Header.module.css";
+import { ReactComponent as Logo } from "../icons/logo.svg";
+import { ReactComponent as Cart } from "../icons/cart.svg";
+import { ReactComponent as Search } from "../icons/search.svg";
+import { ReactComponent as Moon } from "../icons/moon.svg";
+import { ReactComponent as Sun } from "../icons/sun.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <div className={style.container}>
+        {/* navbar-menu */}
+        <nav className={style.menu}>
+          <ul className={style.list}>
+            <li className={style.item}>
+              <a href="#">男款</a>
+            </li>
+            <li className={style.item}>
+              <a href="#">女款</a>
+            </li>
+            <li className={style.item}>
+              <a href="#">最新消息</a>
+            </li>
+            <li className={style.item}>
+              <a href="#">客製商品</a>
+            </li>
+            <li className={style.item}>
+              <a href="#">聯絡我們</a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* nav-logo */}
+        <a href="#" className={style.logo}>
+          <Logo alt="logo" />
+        </a>
+
+        <ul className={style.bar}>
+          {/* search */}
+          <li className={style.item}>
+            <Search alt="Search" />
+          </li>
+          <li className={style.item}>
+            <Cart alt="cart" />
+          </li>
+          {/* <li className={style.item}>
+            <Moon alt="moon" />
+          </li> */}
+          <li className={style.item}>
+            <Sun alt="sun" />
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
