df00d958c520a980ba95034d3c3d980d8d6f6f05
Noseble <manydreamcometure@gmail.com>
2023-02-26 02:37:38 +0800

    Add page turning function

--- src/components/Progress/ProgressControl.js
+++ src/components/Progress/ProgressControl.js
@@ -2,31 +2,32 @@ import { ReactComponent as LeftArrow } from "../assets/icons/left-arrow.svg";
 import { ReactComponent as RightArrow } from "../assets/icons/right-arrow.svg";
 import style from "./ProgressControl.module.css";
 
-export default function ProgressControl() {
+export default function ProgressControl({ ctrl, step }) {
+
   return (
     <section className={style.progress_control}>
-      <section className={style.btn_group} data-phase="address">
-        <button className={style.next}>
+      <section className={`${style.btn_group} ${step === 1 ? '' : style.hidden}`} data-phase="address">
+        <button className={`${style.next} cursor`} onClick={ctrl} data-step='1'>
           下一步
-          <RightArrow className="cursor"/>
+          <RightArrow />
         </button>
       </section>
-      <section className={style.btn_group2} data-phase="shipping">
-        <button className={style.prev}>
-          <LeftArrow className="cursor"/>
+      <section className={`${style.btn_group2} ${step === 2 ? '' : style.hidden}`} data-phase="shipping">
+        <button className={`${style.prev} cursor`} onClick={ctrl} data-step='2'>
+          <LeftArrow />
           上一步
         </button>
-        <button className={style.next}>
+        <button className={`${style.next} cursor`} onClick={ctrl} data-step='1'>
           下一步
-          <RightArrow className="cursor"/>
+          <RightArrow />
         </button>
       </section>
-      <section className={style.btn_group} data-phase="credit-card">
-        <button className={style.prev}>
-          <LeftArrow className="cursor"/>
+      <section className={`${style.btn_group} ${step === 3 ? '' : style.hidden}`} data-phase="credit-card">
+        <button className={`${style.prev} cursor`} onClick={ctrl} data-step='2'>
+          <LeftArrow />
           上一步
         </button>
-        <button className={style.next}>確認下單</button>
+        <button className={`${style.next} cursor`} onClick={ctrl} data-step='3'>確認下單</button>
       </section>
     </section>
   );
--- src/components/RegisterContainer.js
+++ src/components/RegisterContainer.js
@@ -1,18 +1,27 @@
-import style from "./RegistrerContainer.module.css";
+import style from "./RegisterContainer.module.css";
 import Progress from "./Progress/Progress";
 import ProgressControl from "./Progress/ProgressControl";
 import StepOne from "./Step/StepOne";
-// import StepTwo from "./Step/StepTwo";
-// import StepThree from "./Step/StepThree";
+import StepTwo from "./Step/StepTwo";
+import StepThree from "./Step/StepThree";
+import { useState } from "react";
 
-export default function RegistrerContainer() {
+export default function RegisterContainer() {
+  let [state, setState] = useState(1)
+
+  function control(e) {
+    const step = e.target.dataset.step
+    if (step === '1') { setState(state + 1) }
+    if (step === '2') { setState(state - 1) }
+  }
+
   return (
     <div className={style.Container}>
       <Progress />
       <StepOne />
       {/* <StepTwo /> */}
       {/* <StepThree /> */}
-      <ProgressControl />
+      <ProgressControl step={state} ctrl={control} />
     </div>
   );
 }
